7e3cc6bb518107f7dec93839bb3755c62ec5772c
mrv90 <hamid.mousavinasab@gamil.com>
2024-01-07 17:57:53 +0330

    add .env file, which is used on server.js

--- server.js
+++ server.js
@@ -1,1 +1,6 @@
-console.log('hello world');
+const http = require('http');
+require('dotenv').config();
+
+http.createServer().listen(process.env.port, () => {
+  console.log(`Server is listening on ${process.env.port}`);
+});
